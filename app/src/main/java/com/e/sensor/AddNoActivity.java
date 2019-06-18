4ec3fe542fa8001fd55c316ecfd9006fd42e89bb
Softwarica College Lab <softwarica@softwarica.com>
2019-06-18 12:02:04 +0545

    nhmsd

---
+++ app/src/main/java/com/e/sensor/AddNoActivity.java
@@ -0,0 +1,61 @@
+package com.e.sensor;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AddNoActivity extends AppCompatActivity {
+
+    private EditText etFirst, etScond;
+    private Button btnSum;
+    private SensorManager sensorManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_no);
+        setTitle("add two nos using sensor");
+        etFirst = findViewById(R.id.etFirst);
+        etScond = findViewById(R.id.etSecond);
+        btnSum = findViewById(R.id.btnSum);
+        btnSum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add();
+            }
+        });
+        proximity();
+
+
+    }
+
+    private void proximity() {
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        SensorEventListener proxilistener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if (event.values[0] <= 4){
+                    add();
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        sensorManager.registerListener(proxilistener, sensor, SensorManager.SENSOR_DELAY_NORMAL);    }
+
+    private void add(){
+        int sum = Integer.parseInt(etFirst.getText().toString()) + Integer.parseInt(etScond.getText().toString());
+        Toast.makeText(this, "Sum is" + sum, Toast.LENGTH_SHORT).show();
+    }
+}
